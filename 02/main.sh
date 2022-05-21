0239cb9096bc20dc50d534bc1717244840bbddf7
IlyaBykov <97075994+CryptoMagicBoy@users.noreply.github.com>
2022-05-22 00:45:11 +0700

    Delete 02 directory

--- 02/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $# != 0 ]] ; then
-    echo "Incorrect input"
-else
-    chmod +x system_info.sh
-    ./system_info.sh
-fi
--- 02/write.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Write to file? (Yy/N)" log
-
-if [[ $log =~ [Yy] ]]; then
-    name="$(date +%d_%m_%y_%H_%M_%S.status)"
-    ./print.sh > $name
-fi
