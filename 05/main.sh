8e52bd70c4bdc542e87c270c2d520e3d9b0951c7
IlyaBykov <97075994+CryptoMagicBoy@users.noreply.github.com>
2022-07-23 14:42:31 +0700

    reliz

--- 05/main.sh
+++ 05/main.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
-if (( $1 == 1 )) ; then
-    awk {'print | "sort -k8"'} ../04/*.log
-elif (( $1 == 2 )) ; then
-    awk {'print $1 | "sort -u"'} ../04/*.log
-elif (( $1 == 3 )) ; then
-    awk '/ ([4-5]0[0-4]) /{ print }' ../04/*.log
-elif (( $1 == 4 )) ; then
-    awk '/ ([4-5]0[0-4]) /{ print $1 | "sort -u" }' ../04/*.log
+if [[ $# -ne 1 ]] ; then
+    echo "Incorrect input"
+    exit 1
+else
+    if (( $1 == 1 )) ; then
+	awk {'print | "sort -k8"'} ../04/*.log
+    elif (( $1 == 2 )) ; then
+        awk {'print $1 | "sort -u"'} ../04/*.log
+    elif (( $1 == 3 )) ; then
+        awk '/ ([4-5]0[0-4]) /{ print }' ../04/*.log
+    elif (( $1 == 4 )) ; then
+	awk '/ ([4-5]0[0-4]) /{ print $1 | "sort -u" }' ../04/*.log
+    else
+        echo "Incorrect input"
+    fi
 fi
+exit 0
 
